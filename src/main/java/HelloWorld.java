833f8a421d1ece51371f046d6e16168d3ac5a645
Aleksandra Poznanska <alepoznanska@gmail.com>
2020-06-01 23:38:19 +0200

    Initial version

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Aleksandra");
+        System.out.println(28);
+        System.out.println('P');
+    }
+
 }
