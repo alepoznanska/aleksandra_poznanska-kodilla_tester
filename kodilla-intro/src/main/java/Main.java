3675a0129e409a05a2c492d2a64608c0d0b2bcc1
Aleksandra Poznanska <alepoznanska@gmail.com>
2020-06-02 00:02:31 +0200

    Initial version

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Maciej";
+        int myAge = 22;
+        double height = 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        String greeting = "Hello from FirstClass!";
+        System.out.println(myName);
+        System.out.println(greeting);
+        System.out.println(height);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Aleksandra";
+        int myAge = 28;
+        double height = 1.77;
+        char bloodType = 'B';
+        boolean hasBlueEyes = true;
+        String lastName = "Poznanska";
+        System.out.println(myName);
+        System.out.println(lastName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(bloodType);
+        System.out.println(hasBlueEyes);
+    }
+}
