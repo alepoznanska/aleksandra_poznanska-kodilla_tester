703f80f7b0b0241586f3888517cd03c8c9085c5f
Aleksandra Poznanska <alepoznanska@gmail.com>
2020-06-15 22:44:57 +0200

    user

---
+++ kodilla-intro/src/main/java/Grades.java
@@ -0,0 +1,39 @@
+public class Grades {
+    private int[] grades;
+    private int size;
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+    }
+    public void add(int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+
+    }
+
+    public void lastGrade() {
+
+       int ostatniaOcena = grades[this.size-1];
+
+        System.out.println(ostatniaOcena);
+
+
+    }
+    public void averageGrade() {
+        int sumaOcen = 0;
+        for (int i = 0; i < this.size; i++) {
+            sumaOcen = sumaOcen + grades[i];
+
+        }
+        int sredniaOcen = sumaOcen/this.size;
+        System.out.println(sredniaOcen);
+
+
+
+    }
+
+}
