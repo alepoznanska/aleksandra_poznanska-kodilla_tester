e758a088478a059b3c2c93d01717ce3fd1e7010a
Aleksandra Poznanska <alepoznanska@gmail.com>
2020-06-17 20:59:38 +0200

    user

---
+++ kodilla-intro/src/main/java/Application.java
@@ -0,0 +1,23 @@
+public class Application {
+    String name;
+    int age;
+    int height;
+
+    public Application(String name, int age, int height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+    public void checkAgeAndHeight() {
+        if (this.age > 30 && this.height > 160) {
+            System.out.println("User is older than 30 and higher then 160cm");
+        } else {
+            System.out.println("User is younger than 30 or lower than 160cm");
+        }
+    }
+    public static void main(String[] args) {
+        Application adam = new Application("Adam", 50, 177);
+        adam.checkAgeAndHeight();
+
+    }
+}
---
+++ kodilla-intro/src/main/java/GradesPrint.java
@@ -0,0 +1,24 @@
+public class GradesPrint {
+    public static void main(String[] args) {
+        Grades grades = new Grades();
+        grades.add(1);
+
+        grades.add(1);
+
+        grades.add(1);
+
+        grades.add(5);
+
+        grades.add(2);
+        grades.lastGrade();
+        grades.averageGrade();
+
+
+
+
+
+
+
+
+    }
+}
