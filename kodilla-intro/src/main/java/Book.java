2da1fb0f2f6b2cab30c8099373d413aa617ff60c
Aleksandra Poznanska <alepoznanska@gmail.com>
2020-06-17 22:22:29 +0200

    user

---
+++ kodilla-intro/src/main/java/Book.java
@@ -0,0 +1,15 @@
+public class Book {
+    private String author;
+    private String title;
+
+    public static String of(String author, String title) {
+        return author + " to autor książki '" + title + "'";
+
+    }
+    public static void main(String[] args) {
+
+        String book = Book.of("Henryk Sienkiewicz", "W Pustyni i w Puszczy");
+        System.out.println(book);
+
+    }
+}
