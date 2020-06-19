ad0880a1d325c51cfcf6ea387148a74920163a71
Aleksandra Poznanska <alepoznanska@gmail.com>
2020-06-19 07:48:41 +0200

    user

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,25 +1,21 @@
 public class FirstClass {
     public static void main(String[] args) {
         Notebook notebook = new Notebook(600,1000, 2015);
-        System.out.println("A regular notebook weighs " + notebook.weight + "g and costs " + notebook.price + "PLN. It was made in " + notebook.year + ".");
+        Notebook heavyNotebook = new Notebook(2000, 1500, 2020);
+        Notebook oldNotebook = new Notebook(1600,500, 2000);
+        Notebook notTooOldNotebook = new Notebook(1300,1200, 2007);
+
+        displayNotebookParameters(notebook, "A regular notebook weighs ");
+        displayNotebookParameters(heavyNotebook, "A heavy notebook weighs ");
+        displayNotebookParameters(oldNotebook, "An old notebook weighs ");
+        displayNotebookParameters(notTooOldNotebook, "A not-too-old notebook weighs ");
+
+    }
+
+    private static void displayNotebookParameters(Notebook notebook, String s) {
+        System.out.println(s + notebook.weight + "g and costs " + notebook.price + "PLN. It was made in " + notebook.year + ".");
         notebook.checkPrice();
         notebook.checkWeight();
         notebook.checkYearAndPrice();
-        Notebook heavyNotebook = new Notebook(2000, 1500, 2020);
-        System.out.println("A heavy notebook weighs " + heavyNotebook.weight + "g and costs " + heavyNotebook.price + "PLN. It was made in " + heavyNotebook.year + ".");
-        heavyNotebook.checkPrice();
-        heavyNotebook.checkWeight();
-        heavyNotebook.checkYearAndPrice();
-        Notebook oldNotebook = new Notebook(1600,500, 2000);
-        System.out.println("An old notebook weighs " + oldNotebook.weight + "g and costs " + oldNotebook.price + "PLN. It was made in " + oldNotebook.year + ".");
-        oldNotebook.checkPrice();
-        oldNotebook.checkWeight();
-        oldNotebook.checkYearAndPrice();
-        Notebook notTooOldNotebook = new Notebook(1300,1200, 2007);
-        System.out.println("A not-too-old notebook weighs " + notTooOldNotebook.weight + "g and costs " + notTooOldNotebook.price + "PLN. It was made in " + notTooOldNotebook.year + ".");
-        notTooOldNotebook.checkPrice();
-        notTooOldNotebook.checkWeight();
-        notTooOldNotebook.checkYearAndPrice();
-
     }
 }
