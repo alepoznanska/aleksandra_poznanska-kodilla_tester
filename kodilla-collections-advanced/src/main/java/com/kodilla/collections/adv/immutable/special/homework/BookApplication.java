8a2656651d0fe94107101fc37123c47481fdbd20
Aleksandra Poznanska <alepoznanska@gmail.com>
2020-07-23 20:32:10 +0200

    homework_5.4

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
@@ -10,11 +10,11 @@ public class BookApplication {
         books.add(createBook.createBook("Harry Potter And The Sorcerer's Stone", "J.K.Rowling"));
         books.add(createBook.createBook("Harry Potter And The Sorcerer's Stone", "J.K.Rowling"));
         books.add(createBook.createBook("Harry Potter And The Sorcerer's Stone", "J.K.Rowling"));
+        books.add(createBook.createBook("Harry Potter And The Prisoner of Azkaban", "J.K.Rowling"));
 
         for (Book book : books) {
             createBook.createBookList(book);
         }
-
         System.out.println(createBook.books.size());
     }
 }
