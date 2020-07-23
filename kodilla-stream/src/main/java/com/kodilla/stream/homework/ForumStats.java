06744c93ae5ea2031e4925561539d71feca85a51
Aleksandra Poznanska <alepoznanska@gmail.com>
2020-07-23 23:05:43 +0200

    homework_6.3

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -0,0 +1,25 @@
+package com.kodilla.stream.homework;
+
+import com.kodilla.stream.UsersRepository;
+
+public class ForumStats {
+    public static void main(String[] args) {
+        double avg1 = UsersRepository.getUsersList()
+                .stream()
+                .filter(d -> d.getAge() >= 40)
+                .map(u -> u.getNumberOfPosts())
+                .mapToInt(n -> n)
+                .average()
+                .getAsDouble();
+        System.out.println(avg1);
+
+        double avg2 = UsersRepository.getUsersList()
+                .stream()
+                .filter(d -> d.getAge() < 40)
+                .map(u -> u.getNumberOfPosts())
+                .mapToInt(n -> n)
+                .average()
+                .getAsDouble();
+        System.out.println(avg2);
+    }
+}
