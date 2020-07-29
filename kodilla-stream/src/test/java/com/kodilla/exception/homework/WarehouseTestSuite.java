8482d16535e9df72118d0b3ed3be8a391727d693
Aleksandra Poznanska <alepoznanska@gmail.com>
2020-07-29 21:19:36 +0200

    homework_6.5

--- kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
@@ -2,29 +2,42 @@ package com.kodilla.exception.homework;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertNull;
+
 public class WarehouseTestSuite {
+    Warehouse warehouse = new Warehouse();
 
     @Test
     public void testGetOrder() throws OrderDoesntExistException {
         //given
-        Warehouse warehouse = new Warehouse();
-        Order order = new Order("1");
-        warehouse.addOrder(order);
+        warehouse.addOrder("1");
         //when
         Order myOrder = warehouse.getOrder("1");
-        //then
-
     }
 
     @Test(expected = OrderDoesntExistException.class)
     public void testGetOrder_withException() throws OrderDoesntExistException {
         //given
-        Warehouse warehouse = new Warehouse();
-        Order order = new Order("1");
-        warehouse.addOrder(order);
+        warehouse.addOrder("1");
         //when
-        Order myOrder = warehouse.getOrder("67");
+        Order myOrder = warehouse.getOrder("165");
         //then
-        
+        assertNull(myOrder);
+    }
+
+    @Test(expected = OrderDoesntExistException.class)
+    public void getOrderTest_withException() throws Exception {
+
+        System.out.println("Test for exception in method getOrder(). \n"
+                + "given - order numbers 1234, 432, 9876, 567890\n"
+                + "when - ask for order that doesn't exists\n"
+                + "then - throw OrderDoesntExistsException\n");
+        warehouse.addOrder("1234");
+        warehouse.addOrder("432");
+        warehouse.addOrder("9876");
+        warehouse.addOrder("567890");
+        Order order = warehouse.getOrder("dasdasd");
+        System.out.println("dupa blada");
+        assertNull(order);
     }
 }
