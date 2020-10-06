207879c62360eee3cdfb5d567dc31389897293f4
Aleksandra Poznanska <alepoznanska@gmail.com>
2020-10-06 22:17:23 +0200

    13.6

---
+++ kodilla-google-selenium/src/main/java/pages/GoogleSearch.java
@@ -0,0 +1,44 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class GoogleSearch extends AbstractPage {
+    @FindBy(css = "input[title='Szukaj']")
+    static WebElement inputField;
+
+    @FindBy(css = "input[value='Szukaj w Google']")
+    static List<WebElement> searchButton;
+    private static GoogleResults googleResults;
+
+    public GoogleSearch(WebDriver driver) {
+        super(driver);
+    }
+
+    public void searchResults() {
+        //System.setProperty("webdriver.chrome.driver", "C:\selenium-drivers\Chrome\chromedriver.exe");
+        //WebDriver driver = new ChromeDriver();
+        //WebDriverWait wait = new WebDriverWait(driver, 10);
+        //driver.navigate().to("http://www.google.com");
+        PageFactory.initElements(driver, GoogleSearch.class);
+        inputField.sendKeys("Kodilla");
+        googleResults = loadResults(driver);
+        googleResults.iSeeResults();
+        googleResults.clickRandomResults();
+        //wait.until(ExpectedConditions.elementToBeClickable(searchButton.get(0))).click();
+        //driver.close();
+    }
+
+    public static GoogleResults loadResults(WebDriver driver){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.elementToBeClickable(searchButton.get(0))).click();
+        GoogleResults googleResults = new GoogleResults(driver);
+        return googleResults;
+    }
+}
